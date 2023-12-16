f202c0afc8c5804b3c2e85e007868bad66708ff2
Tim <a6987698766@gmail.com>
2023-12-16 16:14:09 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/generator-back": {
-        target: "http://54.199.16.160",
+        target: "http://54.199.16.160:22",
         secure: true,
         changeOrigin: true,
         rewrite: (path) => path,
